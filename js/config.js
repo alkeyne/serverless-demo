a6b61f0105258268e2a4fcbe643f95790a5cc265
alec.madeira <alec.madeira@itech.media>
2022-06-24 11:29:08 +0100

    Config update

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'eu-west-2_4iy87jKIF', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: 'o54qgj5feidtr9i6mj7ffkb0b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'eu-west-2' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
